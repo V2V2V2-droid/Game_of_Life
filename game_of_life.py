31a2c11da0e6e28602031886baf4417af716e2f7
Ms Vigot <victoria.vigot@gmail.com>
2022-06-30 23:17:42 +0100

    First upload of the 2 main files -- clean up one of them

--- game_of_life.py
+++ game_of_life.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import plotly.express as px
 
 standard_rule = "B3S23"  # for Birth = 3 neighbors and Stays alive : 2 or 3
@@ -9,23 +8,24 @@ N = 100
 array_test = np.random.randint(2, size=(N*N)).reshape(N, N)
 array2 = np.random.choice([0, 1], N*N, p=[0.1, 0.9]).reshape(N, N)
 
+
 def neigboors_alive(grid):
-    # for an array return an array of the nb of living neighboors
+    # for an array return an array of the nb of living neighbors
     count_array = np.zeros(shape=grid.shape)
     for (i, j), value in np.ndenumerate(grid):
         array_1 = [(i-1), i, (i+1)]
         array_2 = [(j - 1), j, (j + 1)]
         mesh = np.array(np.meshgrid(array_1, array_2)).T.reshape(-1, 2)
         # remove negative values, remove values superior to N
-        mesh = [(k[0],k[1]) for k in mesh if k[0]>=0 and k[1]>=0 and k[0]<N and k[1]<N]
+        mesh = [(k[0], k[1]) for k in mesh if 0 <= k[0] < N and 0 <= k[1] < N]
         # sum and remove value for the central cell
-        count_array[i,j] = int(sum([grid[c] for c in mesh]) - grid[i,j])
+        count_array[i, j] = int(sum([grid[c] for c in mesh]) - grid[i, j])
     return count_array
 
 
-def update_grid(neighbors, dead_to_alive=[3], staying_alive=[2, 3]):
+def update_grid(neighbors, dead_to_alive, staying_alive):
     # make dict of what should be alive or be reborn based on rules
-    living_rules = {u : 1 if u in staying_alive+dead_to_alive else 0 for u in np.unique(neighbors)}
+    living_rules = {u: 1 if u in staying_alive+dead_to_alive else 0 for u in np.unique(neighbors)}
     # apply on the count of neighbors
     living = np.vectorize(lambda x: living_rules[x])
     # update grid
@@ -34,7 +34,7 @@ def update_grid(neighbors, dead_to_alive=[3], staying_alive=[2, 3]):
 
 class Gol:
 
-    def __init__(self,initial_state, rules, duration=10):
+    def __init__(self, initial_state, rules, duration=10):
         assert isinstance(duration, int) and duration > 1, "duration must be an integer superior to one"
         assert isinstance(initial_state, np.ndarray), "format of the initial board should be a np array"
         self.initial_state = initial_state
@@ -47,21 +47,20 @@ class Gol:
     def parsing_rules(self):
         return [int(i) for i in self.rules.split("B")[1].split("S")[0]], [int(i) for i in self.rules.split("S")[1]]
 
-
     def run_game(self):
         life_journey = np.zeros(shape=(self.duration, self.long, self.width))
-
         for t in range(0, self.duration):
             if t == 0:
                 neighbor_grid = neigboors_alive(grid=self.initial_state)
             else:
                 neighbor_grid = neigboors_alive(grid=life_journey[t-1])
-            life_journey[t, :, :] = update_grid(neighbors=neighbor_grid, dead_to_alive=self.dead_to_alive,staying_alive=self.staying_alive)
+            life_journey[t, :, :] = update_grid(neighbors=neighbor_grid,
+                                                dead_to_alive=self.dead_to_alive, staying_alive=self.staying_alive)
         return life_journey
 
 
 if __name__ == "__main__":
     A = Gol(initial_state=array_test, rules=standard_rule, duration=50).run_game()
-    fig = px.imshow(A, animation_frame=0, binary_string=True,
-                                    labels=dict(animation_frame="time steps in the life journey"))
+    fig = px.imshow(A, animation_frame=0,
+                    binary_string=True, labels=dict(animation_frame="time steps in the life journey"))
     fig.show()
